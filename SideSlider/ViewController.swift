49aa19cf8dd37431fc4a02cbeb8f0930142800e6
Alisher Iglymov <alisher.iglimov.e@gmail.com>
2020-12-15 14:51:47 +0600

    Initial Commit

---
+++ SideSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideSlider
+//
+//  Created by Admin on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
