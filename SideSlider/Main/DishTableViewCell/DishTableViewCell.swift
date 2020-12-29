928854ecf26cbc5f052a2edb900f0bdafee463b4
Alisher Iglymov <alisher.iglimov.e@gmail.com>
2020-12-29 11:40:24 +0600

    Initial implementation of TableView.

--- SideSlider/Main/DishTableViewCell/DishTableViewCell.swift
+++ SideSlider/Main/DishTableViewCell/DishTableViewCell.swift
@@ -9,9 +9,73 @@ import UIKit
 
 class DishTableViewCell: UITableViewCell {
 
+    var dish: Dish? {
+        didSet {
+            dishName.text = dish?.name
+            dishDescription.text = dish?.description
+            dishPrice.text = dish?.price
+        }
+    }
+
+    @IBOutlet weak var innerView: UIView!
+    
+    let dishName: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "GillSansProCyrillic-Medium", size: 18.0)
+        label.textColor = UIColor(red: 39.0/255, green: 65.0/255, blue: 88.0/255, alpha: 1.0)
+        return label
+    }()
+    
+    let dishDescription: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 39.0/255, green: 65.0/255, blue: 88.0/255, alpha: 1.0)
+        label.font = UIFont(name: "GillSansProCyrillic-Light", size: 14.0)
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    let dishPrice: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 39.0/255, green: 65.0/255, blue: 88.0/255, alpha: 1.0)
+        label.font = UIFont(name: "GillSansProCyrillic-Medium", size: 15.0)
+        return label
+    }()
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.selectionStyle = .none
+
+        contentView.backgroundColor = UIColor(red: 47.0/255.0, green: 77.0/255.0, blue: 106.0/255.0, alpha: 1.0)
+        
+        //MARK: innerView
+        innerView.backgroundColor = UIColor(red: 222.0/255, green: 232.0/255, blue: 250.0/255, alpha: 1.0)
+        innerView.layer.cornerRadius = 10
+        innerView.layer.shadowColor = UIColor.black.cgColor
+        innerView.layer.shadowOpacity = 0.1
+        innerView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        innerView.layer.shadowRadius = 1
+        innerView.layer.masksToBounds = false
+        innerView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+
+        //MARK: dishName
+        innerView.addSubview(dishName)
+        dishName.translatesAutoresizingMaskIntoConstraints = false
+        dishName.topAnchor.constraint(equalTo: innerView.topAnchor, constant: 10).isActive = true
+        dishName.trailingAnchor.constraint(equalTo: innerView.trailingAnchor, constant: -15).isActive = true
+        dishName.leadingAnchor.constraint(equalTo: innerView.leadingAnchor, constant: 15).isActive = true
+        
+        //MARK: dishPrice
+        innerView.addSubview(dishPrice)
+        dishPrice.translatesAutoresizingMaskIntoConstraints = false
+        dishPrice.leadingAnchor.constraint(equalTo: innerView.leadingAnchor, constant: 15).isActive = true
+        dishPrice.bottomAnchor.constraint(equalTo: innerView.bottomAnchor, constant: -15).isActive = true
+        
+        //MARK: dishDescription
+        innerView.addSubview(dishDescription)
+        dishDescription.translatesAutoresizingMaskIntoConstraints = false
+        dishDescription.leadingAnchor.constraint(equalTo: innerView.leadingAnchor, constant: 15).isActive = true
+        dishDescription.topAnchor.constraint(equalTo: dishName.bottomAnchor, constant: 15).isActive = true
+        dishDescription.trailingAnchor.constraint(equalTo: innerView.trailingAnchor, constant: -15).isActive = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +83,4 @@ class DishTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
 }
--- SideSlider/Main/ViewController.swift
+++ SideSlider/Main/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     let slider = SliderView(frame: CGRect.zero)
     var scrollTask: DispatchWorkItem?
-    let biscay = UIColor(red: 39.0/255, green: 65.0/255, blue: 88.0/255, alpha: 1.0)
+    let biscay = UIColor(red: 47.0/255, green: 77.0/255, blue: 106.0/255, alpha: 1.0)
     var leadingAnchor: NSLayoutConstraint!
-    var menu: [MenuCategory] = []
+    var menu: [MenuCategory] = [MenuCategory(name: "Sides", description: "Sides", dishes: [Dish(name: "Check", price: "$4.3", description: "Checkcheckcheck", tags: [], id: nil)])]
+    var headerPositions: [CGFloat] = []
+
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -21,6 +23,8 @@ class ViewController: UIViewController {
         
         view.backgroundColor = biscay
         
+        tableView.register(UINib(nibName: "DishTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
+
         //MARK: sliderView
         slider.scrollDelegate = self
         self.slider.sections = ["Check", "That", "View", "Thoroughly", "ERULAN", "AND", "DAULET"]
@@ -34,16 +38,15 @@ class ViewController: UIViewController {
         slider.centerYAnchor.constraint(equalTo: margins.centerYAnchor).isActive = true
     }
 
-
 }
 
 extension ViewController: SliderViewDelegate {
     func scroll(to section: String, with delay: Bool) {
         self.scrollTask?.cancel()
         let task = DispatchWorkItem {
-//            if let section = self.menu.indices.first(where: {self.menu[$0].name == section}) {
-//                self.tableView.scrollToRow(at: IndexPath(row: 0, section: section), at: .top, animated: true)
-//            }
+            if let section = self.menu.indices.first(where: {self.menu[$0].name == section}) {
+                self.tableView.scrollToRow(at: IndexPath(row: 0, section: section), at: .top, animated: true)
+            }
         }
         self.scrollTask = task
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + (delay ? 0.4 : 0.0), execute: task)
@@ -61,7 +64,7 @@ extension ViewController: SliderViewDelegate {
     }
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
+extension ViewController: UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menu[section].dishes.count
     }
@@ -74,5 +77,69 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! DishTableViewCell
         cell.dish = menu[indexPath.section].dishes[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        
+        let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        let attributes = [NSAttributedString.Key.font : UIFont(name: "GaramondPremrPro", size: 24.0) ?? UIFont.systemFont(ofSize: 20.0), NSAttributedString.Key.foregroundColor:  UIColor.white] as [NSAttributedString.Key : Any]
+        let attributedString = NSAttributedString(string: self.menu[section].name!, attributes: attributes)
+        label.attributedText = attributedString
+
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let height = self.menu[section].name!.height(withConstrainedWidth: UIScreen.main.bounds.width, font: UIFont(name: "GaramondPremrPro", size: 24.0) ?? UIFont.systemFont(ofSize: 20.0))
+        return height + 45
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        if tableView.isDragging {
+//            let offset = scrollView.contentOffset.y
+//            let indices = 0...(headerPositions.count - 1)
+//            let section = zip(headerPositions.map({abs($0 - offset)}), indices).min(by: {$0.0 < $1.0})!.1
+//            if headerPositions[section] - offset <= tableView.bounds.height/2 {
+//                self.slider.unselectRest(self.slider.labels[section])
+//            } else if section != 0 {
+//                self.slider.unselectRest(self.slider.labels[section - 1])
+//            }
+//        }
+    }
+    
+    func calculateHeaderPositions() {
+        headerPositions.append(35.0)
+        for category in menu.dropLast() {
+            let height = category.name!.height(withConstrainedWidth: UIScreen.main.bounds.width, font: UIFont(name: "GaramondPremrPro", size: 24.0) ?? UIFont.systemFont(ofSize: 20.0)) + 62.5
+            headerPositions.append(CGFloat(160 * category.dishes.count) + height)
+        }
+        headerPositions = headerPositions
+            .reduce(into: []) { $0.append(($0.last ?? 0) + $1) }
+        print(headerPositions)
+    }
+
+}
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+
+        return ceil(boundingBox.width)
     }
 }
